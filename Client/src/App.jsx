b316907edf6209a5265f247bd27841191dfd21cb
valerydochev <valerisdochev@gmail.com>
2025-12-02 11:54:37 +0000

    Added some components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,36 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Search from "./components/Search"
+import Table from "./components/Table"
+import Pagination from "./components/Pagination"
 
 function App() {
 
   return (
     <div>
-        <div>
-          <Header />
+      <Header />
 
-          <Footer />
-        </div>
+        <main className="main">
+
+            <section className="card users-container">
+              <Search />
+
+              <Table />
+
+              <button className="btn-add btn">Add new user</button>
+
+              <Pagination />
+
+            </section>
+
+          {/* User Details Modal */}
+          {/* Create/Edit Form Modal */}
+          {/* Delete Confirmation Modal */}
+
+        </main>
+
+
+      <Footer />
     </div>
   )
 }
---
+++ Client/src/components/Table.jsx
@@ -0,0 +1,105 @@
+export default function Table() {
+  return (
+    <div className="table-wrapper">
+      
+      {/* Реалната таблица */}
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>
+              First name
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="currentColor"
+                  d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                </path>
+              </svg>
+            </th>
+            <th>
+              Last name
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="currentColor"
+                  d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                </path>
+              </svg>
+            </th>
+            <th>
+              Email
+              <svg className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="currentColor"
+                  d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                </path>
+              </svg>
+            </th>
+            <th>
+              Phone
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="currentColor"
+                  d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                </path>
+              </svg>
+            </th>
+            <th>
+              Created
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                className="icon active-icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="currentColor"
+                  d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                </path>
+              </svg>
+            </th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                alt="Peter's profile"
+                className="image"
+              />
+            </td>
+            <td>Peter</td>
+            <td>Johnson</td>
+            <td>peter@abv.bg</td>
+            <td>0812345678</td>
+            <td>June 28, 2022</td>
+            <td className="actions">
+              <button className="btn edit-btn" title="Edit">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-to-square"
+                  className="svg-inline--fa fa-pen-to-square" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7zM192 64C209.7 64 224 78.33 224 96S209.7 128 192 128H96C78.33 128 64 142.3 64 160V416c0 17.67 14.33 32 32 32h256c17.67 0 32-14.33 32-32V320c0-17.67 14.33-32 32-32s32 14.33 32 32v96c0 53-43 96-96 96H96c-53.02 0-96-43-96-96V160c0-53 43-96 96-96H192z"/>
+                </svg>
+              </button>
+              <button className="btn delete-btn" title="Delete">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash"
+                  className="svg-inline--fa fa-trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="currentColor"
+                    d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2c12.12 0 23.19 6.848 28.62 17.69L320 32h96c17.67 0 32 14.33 32 32s-14.33 32-32 32H32C14.33 96 0 81.67 0 64s14.33-32 32-32h96l7.199-14.31zM31.1 128h416l-22.8 339.1C424.6 492.3 404.1 512 379.9 512H100.1c-24.24 0-44.74-19.69-45.62-44.88L31.1 128z"/>
+                </svg>
+              </button>
+              <button className="btn info-btn" title="Info">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info"
+                  className="svg-inline--fa fa-info" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 192 512">
+                  <path fill="currentColor"
+                    d="M160 448h-32V224c0-17.69-14.33-32-32-32L32 192c-17.67 0-32 14.31-32 32s14.33 32 32 32h32v192H32c-17.67 0-32 14.31-32 32s14.33 32 32 32h128c17.67 0 32-14.31 32-32S177.7 448 160 448zM96 128c26.51 0 48-21.49 48-48S122.5 32.01 96 32.01s-48 21.49-48 48S69.49 128 96 128z"/>
+                </svg>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
