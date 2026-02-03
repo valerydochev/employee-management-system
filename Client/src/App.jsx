d5422dcf704fbf656a141211ab7c5875b39a4b82
valerydochev <valerisdochev@gmail.com>
2026-02-03 09:42:59 +0000

    Implement user form modal for adding and edditing users

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Search from "./components/Search"
 import UserList from "./components/UserList"
 import Pagination from "./components/Pagination"
+import CreateUser from "./components/CreateUser"
 
 function App() {
+    const [showCreateUser, setShowCreateUser] = useState(false);
+
+    const addUserClickHandler = () => {
+      setShowCreateUser(true);
+   } 
 
   return (
     <div>
@@ -17,9 +24,13 @@ function App() {
 
               <UserList />
 
+              <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
+
               <Pagination />
 
             </section>
+
+            {showCreateUser && <CreateUser />}
 
           {/* User Details Modal */}
           {/* Create/Edit Form Modal */}
